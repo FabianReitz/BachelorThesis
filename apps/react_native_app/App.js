492b142883b6493fb6a4c0606e2e30e938473117
FabianReitz <reitz@stadtwerk.org>
2022-05-11 12:30:36 +0200

    [⌨️|react native] Added gyroscope API

--- apps/react_native_app/App.js
+++ apps/react_native_app/App.js
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import { BatteryState } from 'expo-battery';
+import { Gyroscope } from 'expo-sensors';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useBatteryState } from '@use-expo/battery';
@@ -11,12 +13,46 @@ export default function App() {
     // Using @use-expo/battery to get out-of-the-box battery hook.
     const [batteryState] = useBatteryState();
 
+    const [gyroscope, setGyroscope] = useState({
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+
+    const [subscription, setSubscription] = useState(null);
+
+    const _subscribe = () => {
+        setSubscription(
+            Gyroscope.addListener((gyroscopeData) => {
+                setGyroscope(gyroscopeData);
+            })
+        );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
+
+    const { x, y, z } = gyroscope;
+
     return (
         <View style={styles.container}>
             <StatusBar hidden />
             {/* Battery indicator */}
             <Text>Battery:</Text>
-            <Text>{states[batteryState] || ''}</Text>
+            <Text style={{ marginBottom: 20 }}>
+                {states[batteryState] || ''}
+            </Text>
+            <Text>Gyroscope:</Text>
+            <Text>
+                x: {round(x)} y: {round(y)} z: {round(z)}
+            </Text>
         </View>
     );
 }
@@ -36,4 +72,10 @@ const states = {
     [BatteryState.UNPLUGGED]: 'unplugged',
     [BatteryState.CHARGING]: 'charging',
     [BatteryState.FULL]: 'full',
+};
+
+// Round a given number.
+const round = (n) => {
+    if (!n) return 0;
+    return Math.floor(n * 100) / 100;
 };
