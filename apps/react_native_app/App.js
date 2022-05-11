2f3485fac82f43e5147fb927dbfdbd85ce1bafc7
FabianReitz <reitz@stadtwerk.org>
2022-05-11 11:51:10 +0200

    [⌨️|react native] Added Battery state indicator

--- apps/react_native_app/App.js
+++ apps/react_native_app/App.js
@@ -1,20 +1,35 @@
+import { BatteryState } from 'expo-battery';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useBatteryState } from '@use-expo/battery';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // Using @use-expo/battery to get out-of-the-box battery hook.
+    const [batteryState] = useBatteryState();
+
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden />
+            {/* Battery indicator */}
+            <Text>Battery:</Text>
+            <Text>{states[batteryState] || ''}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+// Determin the state of the battery.
+const states = {
+    [BatteryState.UNKNOWN]: 'unkown',
+    [BatteryState.UNPLUGGED]: 'unplugged',
+    [BatteryState.CHARGING]: 'charging',
+    [BatteryState.FULL]: 'full',
+};
